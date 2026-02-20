ed0fe2dc25e3e047d4a12c4516f648ec75984e28
Tim Kelty <tim@craftcms.com>
2026-02-20 12:26:55 -0500

    Allow all CF image props

---
+++ packages/cloud/src/CloudflareImagesTransform.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace craft\cloud;
+
+use Craft;
+use craft\elements\Asset;
+use craft\models\ImageTransform;
+
+class CloudflareImagesTransform
+{
+    /**
+     * @see https://developers.cloudflare.com/images/transform-images/transform-via-workers/#fetch-options
+     * @see https://github.com/cloudflare/workerd/blob/main/types/defines/cf.d.ts
+     *
+     * @param array{color: string, width: int}|array{color: string, top: int, right: int, bottom: int, left: int}|null $border
+     * @param array{url: string, opacity?: float, repeat?: true|'x'|'y', top?: int, left?: int, bottom?: int, right?: int, width?: int, height?: int, fit?: 'scale-down'|'contain'|'cover'|'crop'|'pad'|'squeeze', gravity?: 'face'|'left'|'right'|'top'|'bottom'|'center'|'auto'|'entropy'|array{x?: float, y?: float, mode?: 'remainder'|'box-center'}, background?: string, rotate?: 0|90|180|270|360, segment?: 'foreground'}[]|null $draw
+     * @param 'face'|'left'|'right'|'top'|'bottom'|'center'|'auto'|'entropy'|array{x?: float, y?: float, mode?: 'remainder'|'box-center'}|null $gravity
+     * @param int|'low'|'medium-low'|'medium-high'|'high'|null $quality
+     * @param 'border'|array{top?: int, bottom?: int, left?: int, right?: int, width?: int, height?: int, border?: bool|array{color?: string, tolerance?: int, keep?: int}}|null $trim
+     */
+    public function __construct(
+        public ?bool $anim = null,
+        public ?string $background = null,
+        public ?int $blur = null,
+        public ?array $border = null,
+        public ?float $brightness = null,
+        public ?string $compression = null,
+        public ?float $contrast = null,
+        public ?float $dpr = null,
+        public ?array $draw = null,
+        public ?string $fit = null,
+        public ?string $flip = null,
+        public ?string $format = null,
+        public ?float $gamma = null,
+        public null|string|array $gravity = null,
+        public ?int $height = null,
+        public ?string $metadata = null,
+        public ?string $onerror = null,
+        public ?string $originAuth = null,
+        public null|int|string $quality = null,
+        public ?int $rotate = null,
+        public ?float $saturation = null,
+        public ?string $segment = null,
+        public ?float $sharpen = null,
+        public null|string|array $trim = null,
+        public null|int|string $width = null,
+        public ?float $zoom = null,
+    ) {
+    }
+
+    public static function fromImageTransform(ImageTransform $imageTransform): self
+    {
+        return new self(
+            background: self::resolveBackground($imageTransform),
+            fit: self::resolveFit($imageTransform),
+            format: self::resolveFormat($imageTransform),
+            gravity: self::resolveGravity($imageTransform),
+            height: $imageTransform->height,
+            quality: $imageTransform->quality,
+            width: $imageTransform->width,
+        );
+    }
+
+    private static function resolveFormat(ImageTransform $imageTransform): ?string
+    {
+        if ($imageTransform->format === 'jpg' && $imageTransform->interlace === 'none') {
+            return 'baseline-jpeg';
+        }
+
+        return match ($imageTransform->format) {
+            'jpg' => 'jpeg',
+            default => $imageTransform->format,
+        };
+    }
+
+    /**
+     * @see https://developers.cloudflare.com/images/transform-images/transform-via-url/#fit
+     */
+    private static function resolveFit(ImageTransform $imageTransform): string
+    {
+        // Cloudflare doesn't have an exact match to `stretch`.
+        // `cover` is close, but will crop instead of stretching.
+        return match ($imageTransform->mode) {
+            'fit' => $imageTransform->upscale ? 'contain' : 'scale-down',
+            'stretch' => 'cover',
+            'letterbox' => 'pad',
+            default => $imageTransform->upscale ? 'cover' : 'crop',
+        };
+    }
+
+    private static function resolveBackground(ImageTransform $imageTransform): ?string
+    {
+        return $imageTransform->mode === 'letterbox'
+            ? $imageTransform->fill ?? '#FFFFFF'
+            : null;
+    }
+
+    /**
+     * @return array{x: float, y: float}|null
+     */
+    private static function resolveGravity(ImageTransform $imageTransform): ?array
+    {
+        if ($imageTransform->position === 'center-center') {
+            return null;
+        }
+
+        // TODO: maybe just do this in Craft
+        $parts = explode('-', $imageTransform->position);
+
+        try {
+            $x = match ($parts[1] ?? null) {
+                'left' => 0,
+                'center' => 0.5,
+                'right' => 1,
+            };
+            $y = match ($parts[0] ?? null) {
+                'top' => 0,
+                'center' => 0.5,
+                'bottom' => 1,
+            };
+        } catch (\UnhandledMatchError $e) {
+            Craft::warning("Invalid position value: `{$imageTransform->position}`", __METHOD__);
+            return null;
+        }
+
+        return [
+            'x' => $x,
+            'y' => $y,
+        ];
+    }
+
+    public static function fromAsset(Asset $asset, ?ImageTransform $imageTransform = null): self
+    {
+        $cfTransform = $imageTransform ? self::fromImageTransform($imageTransform) : new self();
+
+        if ($asset->getHasFocalPoint()) {
+            $cfTransform->gravity = $asset->getFocalPoint();
+        }
+
+        return $cfTransform;
+    }
+}
--- packages/cloud/src/ImageTransformer.php
+++ packages/cloud/src/ImageTransformer.php
@@ -10,7 +10,6 @@ use craft\helpers\Assets;
 use craft\helpers\Html;
 use craft\helpers\UrlHelper;
 use craft\models\ImageTransform;
-use Illuminate\Support\Collection;
 use yii\base\NotSupportedException;
 
 /**
@@ -20,7 +19,6 @@ class ImageTransformer extends Component implements ImageTransformerInterface
 {
     public const SUPPORTED_IMAGE_FORMATS = ['jpg', 'jpeg', 'gif', 'png', 'avif', 'webp'];
     private const SIGNING_PARAM = 's';
-    private Asset $asset;
 
     public function init(): void
     {
@@ -29,9 +27,8 @@ class ImageTransformer extends Component implements ImageTransformerInterface
 
     public function getTransformUrl(Asset $asset, ImageTransform $imageTransform, bool $immediately): string
     {
-        $this->asset = $asset;
         $fs = $asset->getVolume()->getTransformFs();
-        $assetUrl = Html::encodeSpaces(Assets::generateUrl($fs, $this->asset));
+        $assetUrl = Html::encodeSpaces(Assets::generateUrl($fs, $asset));
         $mimeType = $asset->getMimeType();
 
         if ($mimeType === 'image/gif' && !Craft::$app->getConfig()->getGeneral()->transformGifs) {
@@ -42,11 +39,14 @@ class ImageTransformer extends Component implements ImageTransformerInterface
             throw new NotSupportedException('SVG files shouldn’t be transformed.');
         }
 
-        $transformParams = $this->buildTransformParams($imageTransform);
+        $cfTransform = CloudflareImagesTransform::fromAsset($asset, $imageTransform);
+
+        $transformParams = array_filter(get_object_vars($cfTransform), fn($v) => $v !== null);
+
         $path = parse_url($assetUrl, PHP_URL_PATH);
         $params = $transformParams + [
-                self::SIGNING_PARAM => $this->sign($path, $transformParams),
-            ];
+            self::SIGNING_PARAM => $this->sign($path, $transformParams),
+        ];
 
         $query = http_build_query($params);
 
@@ -57,87 +57,7 @@ class ImageTransformer extends Component implements ImageTransformerInterface
     {
     }
 
-    private function buildTransformParams(ImageTransform $imageTransform): array
-    {
-        return Collection::make([
-            'width' => $imageTransform->width,
-            'height' => $imageTransform->height,
-            'quality' => $imageTransform->quality,
-            'format' => $this->getFormatValue($imageTransform),
-            'fit' => $this->getFitValue($imageTransform),
-            'background' => $this->getBackgroundValue($imageTransform),
-            'gravity' => $this->getGravityValue($imageTransform),
-        ])->whereNotNull()->all();
-    }
-
-    private function getGravityValue(ImageTransform $imageTransform): ?array
-    {
-        if ($this->asset->getHasFocalPoint()) {
-            return $this->asset->getFocalPoint();
-        }
-
-        if ($imageTransform->position === 'center-center') {
-            return null;
-        }
-
-        // TODO: maybe just do this in Craft
-        $parts = explode('-', $imageTransform->position);
-
-        try {
-            $x = match ($parts[1] ?? null) {
-                'left' => 0,
-                'center' => 0.5,
-                'right' => 1,
-            };
-            $y = match ($parts[0] ?? null) {
-                'top' => 0,
-                'center' => 0.5,
-                'bottom' => 1,
-            };
-        } catch (\UnhandledMatchError $e) {
-            Craft::warning("Invalid position value: `{$imageTransform->position}`", __METHOD__);
-            return null;
-        }
-
-        return [
-            'x' => $x,
-            'y' => $y,
-        ];
-    }
-
-    private function getBackgroundValue(ImageTransform $imageTransform): ?string
-    {
-        return $imageTransform->mode === 'letterbox'
-            ? $imageTransform->fill ?? '#FFFFFF'
-            : null;
-    }
-
-    private function getFitValue(ImageTransform $imageTransform): string
-    {
-        // @see https://developers.cloudflare.com/images/transform-images/transform-via-url/#fit
-        // Cloudflare doesn't have an exact match to `stretch`.
-        // `cover` is close, but will crop instead of stretching.
-        return match ($imageTransform->mode) {
-            'fit' => $imageTransform->upscale ? 'contain' : 'scale-down',
-            'stretch' => 'cover',
-            'letterbox' => 'pad',
-            default => $imageTransform->upscale ? 'cover' : 'crop',
-        };
-    }
-
-    private function getFormatValue(ImageTransform $imageTransform): ?string
-    {
-        if ($imageTransform->format === 'jpg' && $imageTransform->interlace === 'none') {
-            return 'baseline-jpeg';
-        }
-
-        return match ($imageTransform->format) {
-            'jpg' => 'jpeg',
-            default => $imageTransform->format,
-        };
-    }
-
-    private function sign(string $path, $params): string
+    private function sign(string $path, array $params): string
     {
         $paramString = http_build_query($params);
         $data = "$path#?$paramString";
