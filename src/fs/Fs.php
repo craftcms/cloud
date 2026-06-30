cc64df17abe56b1b0d9d2ca09091081c9a1af199
Tim Kelty <tim@craftcms.com>
2026-06-30 07:06:44 -0400

    Log missing image dimensions

--- src/fs/Fs.php
+++ src/fs/Fs.php
@@ -540,6 +540,11 @@ abstract class Fs extends FlysystemFs
             }
         }
 
+        Craft::warning(
+            "Unable to determine image dimensions for \"{$uriPath}\" within the " . self::IMAGE_DIMENSION_MAX_BYTES . ' byte read limit.',
+            __METHOD__,
+        );
+
         return null;
     }
 
