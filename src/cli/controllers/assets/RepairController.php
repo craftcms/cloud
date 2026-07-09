019b67c5245f84f6a4ad02a4b5ea8eb65099af16
Tim Kelty <tim@craftcms.com>
2026-07-09 11:02:57 -0400

    Limit repair controller actions

--- src/cli/controllers/assets/RepairController.php
+++ src/cli/controllers/assets/RepairController.php
@@ -8,6 +8,15 @@ class RepairController extends AssetsController
 {
     public $defaultAction = 'missing';
 
+    public function createAction($id)
+    {
+        if (!in_array($id, ['missing', 'metadata'], true)) {
+            return null;
+        }
+
+        return parent::createAction($id);
+    }
+
     public function actionMissing(): int
     {
         return $this->repairMissingAssetData();
