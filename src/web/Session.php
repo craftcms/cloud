bf04c930721ccbab30985ed90244c50dae020af8
Tim Kelty <tim@craftcms.com>
2026-07-09 07:26:46 -0400

    CLOUD-701 Log session opens (#179)
    * CLOUD-701 Add cache causal logging
    
    * CLOUD-701 Narrow session cache logging
    
    * CLOUD-701 Redact session cookie logs
    
    * CLOUD-701 Drop AppConfig session test
    
    * CLOUD-701 Log only session stacks
    
    * CLOUD-701 Bound session stack logging
    
    * CLOUD-701 Resolve session template context
    
    * CLOUD-701 Rename session close log
    
    * CLOUD-701 Keep session logs stack-only
    
    * CLOUD-701 Clarify session log guards
    
    * CLOUD-701 Verify session close transition
    
    * CLOUD-701 Use session state snapshots
    
    * CLOUD-701 Drop session close logging

---
+++ src/web/Session.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace craft\cloud\web;
+
+use Craft;
+use craft\helpers\App;
+use samdark\log\PsrMessage;
+use yii\web\DbSession;
+
+class Session extends DbSession
+{
+    public function open()
+    {
+        $wasActive = $this->getIsActive();
+
+        parent::open();
+
+        if ($wasActive || !$this->getIsActive()) {
+            return;
+        }
+
+        Craft::info(new PsrMessage(
+            'Session opened',
+            [
+                'stack' => App::backtrace(8),
+            ],
+        ), __METHOD__);
+    }
+}
