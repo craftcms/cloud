8e00847caac870186b39e92107b873024b93f006
Tim Kelty <tim@craftcms.com>
2026-07-24 09:47:07 -0400

    Improve static cache purge diagnostics (#194)
    * Improve static cache purge diagnostics
    
    * Isolate request context test
    
    * Simplify web request check
    
    * Use request type for URL context
    
    * Preserve configured test request URI
    
    * Log collected purge inputs consistently

---
+++ tests/unit/ModuleTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace craft\cloud\tests\unit;
+
+use Codeception\Test\Unit;
+use Craft;
+use craft\cloud\Module;
+use craft\web\Request;
+use samdark\log\PsrMessage;
+use yii\log\Logger;
+
+class ModuleTest extends Unit
+{
+    public function testLogAddsRequestContext(): void
+    {
+        $originalLogger = Craft::getLogger();
+        $originalRequest = Craft::$app->getRequest();
+        $logger = new Logger(['flushInterval' => 0]);
+        $request = new Request();
+        $route = Craft::$app->requestedRoute;
+        $params = Craft::$app->requestedParams;
+
+        try {
+            Craft::setLogger($logger);
+            Craft::$app->set('request', $request);
+            Craft::$app->requestedRoute = 'templates/render';
+            Craft::$app->requestedParams = ['template' => 'news/_entry'];
+            $request->setHostInfo('https://example.com');
+            $request->setUrl('/news/example?token=x&filter=recent');
+            $request->setIsConsoleRequest(false);
+
+            Module::info('Test', ['existing' => true]);
+            $message = $logger->messages[array_key_last($logger->messages)][0];
+        } finally {
+            Craft::setLogger($originalLogger);
+            Craft::$app->set('request', $originalRequest);
+            Craft::$app->requestedRoute = $route;
+            Craft::$app->requestedParams = $params;
+        }
+
+        $this->assertInstanceOf(PsrMessage::class, $message);
+        $this->assertSame([
+            'existing' => true,
+            'requestedRoute' => 'templates/render',
+            'requestedParams' => ['template' => 'news/_entry'],
+            'url' => 'https://example.com/news/example?token=x&filter=recent',
+        ], $message->getContext());
+    }
+}
